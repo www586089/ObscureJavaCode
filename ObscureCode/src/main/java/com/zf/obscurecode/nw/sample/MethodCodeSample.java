b88c0660e26fa3d40b139dc8e40e3ca71a04bfb0
www586089 <414929680@qq.com>
2019-06-26 09:47:44 +0800

    MethodCodeSample

---
+++ ObscureCode/src/main/java/com/zf/obscurecode/nw/sample/MethodCodeSample.java
@@ -0,0 +1,20 @@
+package com.zf.obscurecode.nw.sample;
+
+import com.zf.obscurecode.nw.source.CodeType;
+
+public class MethodCodeSample extends BaseCodeSample {
+
+    private String methodName;
+    @Override
+    public CodeType getType() {
+        return CodeType.METHOD;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+}
